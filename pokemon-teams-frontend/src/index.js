6f61b2c8909e6e4c1d579c3faa2d575c26cb84ea
gerardmi24 <gerardmistretta@yahoo.com>
2020-11-02 14:52:36 -0500

    Finished

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,18 +1,120 @@
+// DOM ELEMENTS
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
 
+// ✅ When a user loads the page, they should see all trainers, with their current team of Pokemon.
+// ✅ Whenever a user hits Add Pokemon and they have space on their team, they should get a new Pokemon.
+// ✅ Whenever a user hits Release Pokemon on a specific Pokemon team, that specific Pokemon should be released from the team.
 
-// When a user loads the page, they should see all trainers, with their current team of Pokemon.
-// Whenever a user hits Add Pokemon and they have space on their team, they should get a new Pokemon.
-// Whenever a user hits Release Pokemon on a specific Pokemon team, that specific Pokemon should be released from the team.
+// EVENT LISTENERS
+main.addEventListener("click", event => {
+    if (event.target.matches(".add-button")) {
+        addPokemonToParty(event)
+    } else if (event.target.matches(".release")) {
+        releasePokemonToWild(event)
+    }
+})
+
+// EVENT HANDLERS
+function addPokemonToParty(event) {
+    const trainerId = event.target.dataset.trainerId
+    const trainerUl = event.target.nextSibling
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ "trainer_id": `${trainerId}`})
+    }
+    
+    fetch(POKEMONS_URL, options)
+        .then(resp => resp.json())
+        .then(newPokemonData => {
+            console.log(newPokemonData)
+            //render new pokemon
+            const newPokemonLi = renderPokemon(newPokemonData)
+            //append to correct Ul if not error
+            if (newPokemonData.error === "Party is Full!") {
+                alert("Party is Full!")
+            } else if (newPokemonData.error === "Trainer not found") {
+                alert("Trainer not found")
+            } else {
+                trainerUl.append(newPokemonLi)
+            }
+        })
+}
+
+function releasePokemonToWild(event) {
+    const pokeId = event.target.dataset.pokemonId
+    //remove from the DOM
+    event.target.closest("li").remove()
+
+    const options = {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+
+    fetch(`${POKEMONS_URL}/${pokeId}`, options)
+        .then(resp => resp.json())
+        .then(data => {console.log(data)})    
+}
+
+// DOM RENDERING
+// returns a new LI that needs to then be appended
+function renderPokemon(pokemon) {
+    const newLi = document.createElement('li')
+    const nickname = pokemon.nickname
+    const species = pokemon.species
+    const pokeID = pokemon.id
+    newLi.textContent = `${nickname} (${species})`
+    //release button
+    const releaseButton = document.createElement('button')
+    releaseButton.classList.add("release")
+    releaseButton.dataset.pokemonId = pokeID
+    releaseButton.textContent = "Release"
+    //append button
+    newLi.append(releaseButton)
+    return newLi
+}
 
 function renderTrainers(trainersData) { 
     trainersData.forEach(renderTrainer)
 }
 
 function renderTrainer(trainer) {
-    console.log("render single trainer",trainer)
+    //create a single trainer card !!!
+    const trainerCard = document.createElement('div')
+    trainerCard.classList.add("card")
+    trainerCard.dataset.id = trainer.id
+    //make paragraph
+    const trainerNameP = document.createElement('p')
+    trainerNameP.textContent = trainer.name
+    //make button
+    const addPokemonButton = document.createElement('button')
+    addPokemonButton.dataset.trainerId = trainer.id
+    addPokemonButton.textContent = "Add Pokemon"
+    addPokemonButton.classList.add("add-button")
+
+    //nested version of adding an event listener
+    // addPokemonButton.addEventListener("click", addPokemonToParty)
+
+    //pokemon UL
+    const pokemonPartyUl = document.createElement('ul')
+    //create LIs for inside of each UL
+    trainer.pokemons.forEach((pokemon) => {
+        const renderedPokemonLi = renderPokemon(pokemon)
+        pokemonPartyUl.append(renderedPokemonLi)
+    })
+    //append all to trainer card
+    trainerCard.append(trainerNameP, addPokemonButton, pokemonPartyUl)
+    //append to <main>
+    main.append(trainerCard)
 }
 
 function initialize() {
@@ -24,15 +126,3 @@ function initialize() {
 }
 
 initialize()
-
-//ideal pokemond trainer card
-/* <div class="card" data-id="1"><p>Prince</p>
-  <button data-trainer-id="1">Add Pokemon</button>
-  <ul>
-    <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
-    <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-    <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-    <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-    <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
-  </ul>
-</div> */
